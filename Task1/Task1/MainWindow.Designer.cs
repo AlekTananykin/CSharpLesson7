eaef66198620b889bab94bd30f350ac27f8d3123
AlekTananykin <tananykin@mail.ru>
2020-11-16 00:21:12 +0300

    TargetNumber in state

--- Task1/Task1/MainWindow.Designer.cs
+++ Task1/Task1/MainWindow.Designer.cs
@@ -32,11 +32,18 @@
             this.btnMultiply = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.lblNumbers = new System.Windows.Forms.Label();
+            this.lblCommandsCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPlayGame = new System.Windows.Forms.Button();
+            this.lblCommandsCountText = new System.Windows.Forms.Label();
+            this.lblTargetNumber = new System.Windows.Forms.Label();
+            this.btnUndo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnIncreament
             // 
-            this.btnIncreament.Location = new System.Drawing.Point(241, 47);
+            this.btnIncreament.Location = new System.Drawing.Point(16, 12);
             this.btnIncreament.Name = "btnIncreament";
             this.btnIncreament.Size = new System.Drawing.Size(130, 50);
             this.btnIncreament.TabIndex = 0;
@@ -46,7 +53,7 @@
             // 
             // btnMultiply
             // 
-            this.btnMultiply.Location = new System.Drawing.Point(241, 113);
+            this.btnMultiply.Location = new System.Drawing.Point(152, 12);
             this.btnMultiply.Name = "btnMultiply";
             this.btnMultiply.Size = new System.Drawing.Size(130, 50);
             this.btnMultiply.TabIndex = 1;
@@ -56,7 +63,7 @@
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(241, 191);
+            this.btnReset.Location = new System.Drawing.Point(288, 12);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(130, 50);
             this.btnReset.TabIndex = 2;
@@ -67,17 +74,91 @@
             // lblNumbers
             // 
             this.lblNumbers.AutoSize = true;
-            this.lblNumbers.Location = new System.Drawing.Point(45, 47);
+            this.lblNumbers.Location = new System.Drawing.Point(284, 100);
             this.lblNumbers.Name = "lblNumbers";
             this.lblNumbers.Size = new System.Drawing.Size(18, 20);
             this.lblNumbers.TabIndex = 3;
             this.lblNumbers.Text = "0";
             // 
+            // lblCommandsCount
+            // 
+            this.lblCommandsCount.AutoSize = true;
+            this.lblCommandsCount.Location = new System.Drawing.Point(284, 174);
+            this.lblCommandsCount.Name = "lblCommandsCount";
+            this.lblCommandsCount.Size = new System.Drawing.Size(18, 20);
+            this.lblCommandsCount.TabIndex = 4;
+            this.lblCommandsCount.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество команд";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Число";
+            // 
+            // btnPlayGame
+            // 
+            this.btnPlayGame.Location = new System.Drawing.Point(424, 12);
+            this.btnPlayGame.Name = "btnPlayGame";
+            this.btnPlayGame.Size = new System.Drawing.Size(130, 50);
+            this.btnPlayGame.TabIndex = 7;
+            this.btnPlayGame.Text = "Играть";
+            this.btnPlayGame.UseVisualStyleBackColor = true;
+            this.btnPlayGame.Click += new System.EventHandler(this.btnPlayGame_Click);
+            // 
+            // lblCommandsCountText
+            // 
+            this.lblCommandsCountText.AutoSize = true;
+            this.lblCommandsCountText.Location = new System.Drawing.Point(12, 133);
+            this.lblCommandsCountText.Name = "lblCommandsCountText";
+            this.lblCommandsCountText.Size = new System.Drawing.Size(244, 20);
+            this.lblCommandsCountText.TabIndex = 8;
+            this.lblCommandsCountText.Text = "Число, которое надо получить";
+            this.lblCommandsCountText.Visible = false;
+            // 
+            // lblTargetNumber
+            // 
+            this.lblTargetNumber.AutoSize = true;
+            this.lblTargetNumber.Location = new System.Drawing.Point(284, 133);
+            this.lblTargetNumber.Name = "lblTargetNumber";
+            this.lblTargetNumber.Size = new System.Drawing.Size(18, 20);
+            this.lblTargetNumber.TabIndex = 9;
+            this.lblTargetNumber.Text = "0";
+            this.lblTargetNumber.Visible = false;
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(560, 12);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(130, 50);
+            this.btnUndo.TabIndex = 10;
+            this.btnUndo.Text = "Отменить";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(399, 294);
+            this.ClientSize = new System.Drawing.Size(776, 464);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.lblTargetNumber);
+            this.Controls.Add(this.lblCommandsCountText);
+            this.Controls.Add(this.btnPlayGame);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCommandsCount);
             this.Controls.Add(this.lblNumbers);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnMultiply);
@@ -95,6 +176,13 @@
         private System.Windows.Forms.Button btnMultiply;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Label lblNumbers;
+        private System.Windows.Forms.Label lblCommandsCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPlayGame;
+        private System.Windows.Forms.Label lblCommandsCountText;
+        private System.Windows.Forms.Label lblTargetNumber;
+        private System.Windows.Forms.Button btnUndo;
     }
 }
 
--- Task1/Task1/MainWindow.cs
+++ Task1/Task1/MainWindow.cs
@@ -8,6 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*Тананыкин
+ * 
+ * а) Добавить в программу «Удвоитель» подсчёт количества отданных команд 
+ * удвоителю.
+ * б) Добавить меню и команду «Играть». При нажатии появляется сообщение, 
+ * какое число должен получить игрок. Игрок должен получить это число за 
+ * минимальное количество ходов.
+ * в) *Добавить кнопку «Отменить», которая отменяет последние ходы. 
+ * Используйте обобщенный класс Stack.
+ * Вся логика игры должна быть реализована в классе с удвоителем.
+ */
+
 namespace Task1
 {
     public partial class MainWindow : Form
@@ -19,17 +31,89 @@ namespace Task1
 
         private void btnIncreament_Click(object sender, EventArgs e)
         {
-            lblNumbers.Text = (int.Parse(lblNumbers.Text) + 1).ToString();
+            lblNumbers.Text = (long.Parse(lblNumbers.Text) + 1).ToString();
+            IncrementCommandsCount();
+            PushState();
         }
 
         private void btnMultiply_Click(object sender, EventArgs e)
         {
-            lblNumbers.Text = (int.Parse(lblNumbers.Text) * 2).ToString();
+            
+            lblNumbers.Text = (long.Parse(lblNumbers.Text) * 2).ToString();
+            IncrementCommandsCount();
+            PushState();
         }
 
         private void btnReset_Click(object sender, EventArgs e)
         {
             lblNumbers.Text = "0";
+            IncrementCommandsCount();
+            PushState();
+        }
+
+        private void IncrementCommandsCount()
+        {
+            lblCommandsCount.Text = 
+                (long.Parse(lblCommandsCount.Text) + 1).ToString();
+        }
+
+        private void btnPlayGame_Click(object sender, EventArgs e)
+        {
+            lblNumbers.Text = "0";
+            lblCommandsCount.Text = "0";
+            Random rnd = new Random();
+            lblTargetNumber.Text = Math.Abs(rnd.Next()).ToString();
+
+            lblTargetNumber.Visible = true;
+            lblCommandsCountText.Visible = true;
+        }
+
+        private struct GameState
+        {
+            public string Number { get; set; }
+            public string TargetNumber { get; set; }
+            public string CommandsCount { get; set; }
+        }
+
+        Stack<GameState> _statesStack = new Stack<GameState>();
+
+        private void PushState()
+        {
+            _statesStack.Push(new GameState()
+            {
+                Number = lblNumbers.Text,
+                TargetNumber = (lblTargetNumber.Visible)? 
+                    lblTargetNumber.Text: String.Empty,
+                CommandsCount = lblCommandsCount.Text
+            });
+        }
+
+        private void PopState()
+        {
+            if (2 > _statesStack.Count)
+                return;
+
+            _statesStack.Pop();
+            GameState state = _statesStack.Peek();
+
+            if (0 == state.TargetNumber.Length)
+            {
+                lblTargetNumber.Visible = false;
+                lblCommandsCountText.Visible = false;
+            }
+            else
+            {
+                lblTargetNumber.Visible = true;
+                lblCommandsCountText.Visible = true;
+                lblCommandsCountText.Text = state.TargetNumber;
+            }
+            lblNumbers.Text = state.Number;
+            lblCommandsCount.Text = state.CommandsCount;
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            PopState();
         }
     }
 }
